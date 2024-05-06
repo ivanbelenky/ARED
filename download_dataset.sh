800f3039f2546e17484f358cba125d77356e9ef0
ivan <49297252+ivanbelenky@users.noreply.github.com>
2024-05-06 06:49:24 +0200

    Update download_dataset.sh

--- download_dataset.sh
+++ download_dataset.sh
@@ -1,6 +1,7 @@
 #! /bin/sh
-wget -O ARED0.tar.gz https://ared0.s3.amazonaws.com/ARED0.tar.gz
-wget -O ARED0.csv https://ared0.s3.amazonaws.com/ARED0.csv
-tar -xvf ARED0.tar.gz -C .
-mv dataset/ARED0.csv dataset/ARED0.csv
-rm ARED0.tar.gz
+# wget -O ARED0.tar.gz https://ared0.s3.amazonaws.com/ARED0.tar.gz
+# wget -O ARED0.csv https://ared0.s3.amazonaws.com/ARED0.csv
+# tar -xvf ARED0.tar.gz -C .
+# mv dataset/ARED0.csv dataset/ARED0.csv
+# rm ARED0.tar.gz
+echo "static files have been removed, contact ivanbelenky at gmail.com"
